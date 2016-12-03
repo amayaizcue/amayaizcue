6ffaebbf8b7ea92bff0932279eefeae04a211b23
Eduardo Alfaro <eduardo.alfaro@outlook.com>
2016-12-03 14:09:17 +0100

    Ajustes de la animación.

--- web/js/jquery.magnific-popup.js
+++ web/js/jquery.magnific-popup.js
@@ -1,9 +1,6 @@
-/*
-Author: Amaya Izcue Chourraut
-Description: Personal Website and Portfolio
-Date: 2016
-*/
-
+/*! Magnific Popup - v1.1.0 - 2016-02-20
+* http://dimsemenov.com/plugins/magnific-popup/
+* Copyright (c) 2016 Dmitry Semenov; */
 ; (function (factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module. 
