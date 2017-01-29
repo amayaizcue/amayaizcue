97440bff20473eb2ed356de6617f31f388ef34ec
Eduardo Alfaro <eduardo.alfaro@outlook.com>
2017-01-29 11:52:34 +0100

    Ordenación de ficheros PHP.

--- web/notifyMe.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-		$senderEmail=$_POST["senderEmail"];
-
-		$mailBody="Email: $senderEmail";
-
-		mail("amayaizcue@gmail.com", "Subject", $mailBody, "From: prueba <$senderEmail>");
-
-    ?>
